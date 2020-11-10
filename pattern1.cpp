f92bafeb2d0d897dddc08e869d9346212e560156
kola <kolareddy.nagendra2018@vitstudent.ac.in>
2020-11-10 14:42:10 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num,n;
+    int k=0;
+    cout<<"enter number of pyramids:";
+    cin>>num;
+    while(num!=0){
+    cout<<"enter number of steps:";
+    cin>>n;
+    for(int i=0;i<=n-1;i++){
+        for(int j=0;j<=i;j++){
+            cout<<k;
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    num--;
+}
+return 0;
+}
